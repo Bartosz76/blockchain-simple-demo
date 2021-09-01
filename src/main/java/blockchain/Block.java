a6331a7d042177ecc04f0facd044b4d17807030e
bmierzwinski <Bartosz.Mierzwinski@cedc.com>
2021-09-01 10:24:44 +0200

    Created the Block's model.

---
+++ src/main/java/blockchain/Block.java
@@ -0,0 +1,36 @@
+package blockchain;
+
+import java.util.Arrays;
+
+public class Block {
+
+    private int previousHash;
+    private String[] transactions;
+    private int blockHash;
+
+    public Block(int previousHash, String[] transactions) {
+        this.previousHash = previousHash;
+        this.transactions = transactions;
+
+        /**
+         * Calculating the hash of the current block based on the hash of all of the transactions and the previous hash.
+         * Contents is the hash of the transactions that are included in the current block and the hash of the previous
+         * block.
+         */
+        Object[] contents = {Arrays.hashCode(transactions), previousHash};
+        /**
+         * The current block's hash is based on contents (explained above). So if anything is changed in the
+         * transactions or in the transactions in the previous block or anything in the transactions of the block
+         * before the previous block... this.blockHash will be completely different.
+         */
+        this.blockHash = Arrays.hashCode(contents);
+    }
+
+    public int getPreviousHash() {
+        return previousHash;
+    }
+
+    public String[] getTransactions() {
+        return transactions;
+    }
+}
---
+++ src/main/java/blockchain/Main.java
@@ -0,0 +1,30 @@
+package blockchain;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /**
+         * Hash is a digital signature of data.
+         */
+
+        /**
+         * Each block of the blockchain will have a list of transactions and a previous hash.
+         */
+
+        /**
+         * Below two arrays will have an identical hash. Because they are identical.
+         * String[] arrayOne = {"a", "b", "c"};
+         * String[] arrayTwo = {"a", "b", "c"};
+         *
+         * If I make a change: arrayTwo = {"ab", "b", "c"};...
+         * The hash of this array will change. This is the foundation of a blockchain - every block of the chain
+         * holds the digital signature of the previous block and the following block's digital signature depends
+         * on the current one's hash. To meddle with one's hash means ruining the hashes of all the following ones.
+         */
+
+
+
+
+    }
+}
